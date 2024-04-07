5071d07636c4adcc3a33b2a0dc22c84d7682399c
Anrean-L <anrean_augusta@vk.com>
2024-04-07 13:31:19 +0300

    Add events rendering

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ let form = document.forms[0];
 form.date.value = now.toISOString().substring(0, now.toISOString().length - 5);
 form.addEventListener('submit', (e) => {
     e.preventDefault();
+    console.log(form.checkValidity());
     let eventsList = JSON.parse(localStorage.getItem('events')) || [];
     eventsList.push({
         'title': form.title.value,
@@ -13,18 +14,29 @@ form.addEventListener('submit', (e) => {
     });
     localStorage.setItem('events', JSON.stringify(eventsList));
     form.parentNode.classList.toggle('add-event--opened');
+    renderEvents();
 });
 
-let eventsList = JSON.parse(localStorage.getItem('events')) || [];
-eventsList.forEach(item => {
-    let date = new Date(item.date);
-    document.getElementsByClassName('events-list')[0].insertAdjacentHTML('beforeend', `
+function renderEvents() {
+    let eventsList = JSON.parse(localStorage.getItem('events')) || [];
+    eventsList.forEach((item, index) => {
+        let date = new Date(item.date);
+        let distinction = new Date(date - new Date());
+        document.getElementsByClassName('events-list')[0].insertAdjacentHTML('beforeend', `
     <div class="events-list__item event">
         <h2 class="event__title">${item.title}</h2>
-        <time class="event__date" datetime="2025-01-01T00:00:00+05:00">${date}</time>
+        <time class="event__date" datetime="2025-01-01T00:00:00+05:00">${date.toLocaleString()}</time>
         <div class="event__time-container">
-            <div class="event__big-date">26 дней</div>
-            <div class="event__small-date">09:16:35</div>
+            ${distinction >= 86400000 || distinction <= -86400000 ? `<div class="event__big-date">${((date - now) / 86400000).toFixed(0)} дней</div>` : '<div class="event__big-date"></div>'}
+            <div class="event__small-date">${distinction.toLocaleTimeString()}</div>
         </div>
-    </div>`)
-});
+    </div>`);
+        setInterval(() => {
+            let distinction = new Date(date - new Date());
+            document.getElementsByClassName('event__small-date')[index].innerHTML = distinction.toLocaleTimeString();
+            document.getElementsByClassName('event__big-date')[index].innerHTML = distinction >= 86400000 || distinction <= -86400000 ? `<div class="event__big-date">${((date - now) / 86400000).toFixed(0)} дней</div>` : '';
+        }, 1000);
+    });
+}
+
+renderEvents();
